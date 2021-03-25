97514b7deb9a783dcb38acd84420748c96b1032a
shimozuki <robbishimozuki@gmail.com>
2021-03-25 18:21:13 +0800

    animatedcontainer & gesturedetector

---
+++ android/app/src/main/kotlin/com/example/animatedcontainer_gesturedetector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animatedcontainer_gesturedetector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("AnimatedContainer & GestureDetector"),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              color: Color.fromARGB(255, random.nextInt(256),
+                  random.nextInt(256), random.nextInt(256)),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
